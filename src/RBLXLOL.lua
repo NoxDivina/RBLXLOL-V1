754d0b47486e6b4130ed8af3d7df78b8f6c818b8
Nox Divina <75749401+NoxDivina@users.noreply.github.com>
2026-06-30 23:27:16 +0300

    Update RBLXLOL.lua

--- src/RBLXLOL.lua
+++ src/RBLXLOL.lua
@@ -30,7 +30,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
            Title = "KeySys",
            Subtitle = "Key System",
            Note = "You can find the key at github.com/NoxDivina/RBLXLOL-V1", -- Use this to tell the user how to get a key
-           FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+           FileName = "RblxLOLKey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
            SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
            GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
            Key = {"Tempus finitum est, sapienter utere"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
@@ -43,7 +43,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
      local Tab2 = Window:CreateTab("Advanced Scripts", "terminal") -- Title, Image
      local Tab3 = Window:CreateTab("Troll Scripts", "skull") -- Title, Image
      local Tab4 = Window:CreateTab("Keybinds", "keyboard") -- Title, Image
-
+-- local RivalsTab = Window:CreateTab("Rivals", "placeimagehere") --
      -- Sliders --
 
      local Slider = Tab:CreateSlider({
